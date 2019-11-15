7032d5d093f53e7b6355beb62c99d53da0709689
Doreh Kazemi <dkazemis@eng.ucsd..edu>
2019-11-15 09:02:04 -0800

    Adding selenium code

---
+++ Amazon-Prj/scrapyexample/spiders/wiki_spider_merge.py
@@ -0,0 +1,57 @@
+import scrapy
+
+class WikiSpider(scrapy.Spider):
+
+    name = "wiki_company_merge"
+
+    def start_requests(self):
+        urls = [
+            'https://en.wikipedia.org/wiki/List_of_mergers_and_acquisitions_by_Amazon',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        strings = []
+        for i in range(0, 100):
+            try:
+                for node in response.xpath('//*[@id="mw-content-text"]/div/p[{}]'.format(i)):
+                    text = (node.xpath('string()').extract())
+                    if len(text):
+                        strings.append(text)
+            except Exception as error:
+                strings.append(str(error))
+
+
+        rows = response.xpath("//*[@id='mw-content-text']/div/table[contains(@class, 'wikitable sortable')]/tbody/tr")
+        table = {}
+        for row in rows:
+            columns = row.css('td')
+            if(len(columns) == 7):
+                company = columns[1].css('::text').extract_first()
+                date = columns[3].css('::text').extract_first()
+                amount = columns[4].css('::text').extract_first()
+
+                table['company'] = company.strip()
+                table['date'] = date.strip()
+                table['amount'] = date.strip()
+
+                print(company, amount, date)
+
+                yield{
+                    **table
+                }
+
+                for next_page in row.css('td > a'):
+                    yield response.follow(next_page, self.parse)
+
+        yield{
+             'Title': response.css('#firstheading::text').extract_first(),
+            'Paragraphs': strings
+        }
+
+
+        return table
+
+
