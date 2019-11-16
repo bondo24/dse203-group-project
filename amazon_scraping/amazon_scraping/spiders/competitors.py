d7c5d233581709eb1d0b1cf21b7f4aff86b586f3
Matt Bond <m1bond@eng.ucsd.edu>
2019-11-16 09:08:50 -0800

    updated competitors

--- amazon_scraping/amazon_scraping/spiders/competitors.py
+++ amazon_scraping/amazon_scraping/spiders/competitors.py
@@ -21,16 +21,51 @@ class CompetitorsSpider(scrapy.Spider):
         self.output['competitors'][title] = {}
         competitor = self.output['competitors'][title]
         competitor['organization'] = infobox_xpath.xpath('./caption/text()').get()
-        competitor['founded'] = re.sub(r'[^\x00-\x7F]+',' ', infobox_xpath.xpath('./tbody/tr[*]/th[contains(text(), "Founded")]/../td/text()').get())
-        competitor['industry'] = infobox_xpath.xpath('./tbody/tr[*]/th[contains(text(), "Industry")]/../td[@class="category"]//text()').getall()
-        # clean industry values
-        competitor['industry'] = list(filter(lambda text: text != '\n', competitor['industry']))
-        # some don't have products
-        try:
-            competitor['products'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Products")]/ancestor::tr/td/text()').get().split(', ')
-        except AttributeError:
-            competitor['products'] = None
-        competitor['number_of_employees'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Number of employees") or contains(text(), "Employees")]/ancestor::tr/td/text()').get()
-        competitor['number_of_employees'] = re.sub('[^0-9]','', competitor['number_of_employees'])
-        # acquisition['location'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Headquarters")]/ancestor::tr/td/string()').get()
-        competitor['summary'] = ''.join(response.xpath('//*[@id="mw-content-text"]/div/p[*]/b[contains(text(), "{}")]/..//text()'.format(title)).getall())
+
+        if competitor['organization'] == 'Etsy':
+            competitor['founded'] = re.sub(r'[^\x00-\x7F]+',' ', infobox_xpath.xpath('./tbody/tr[*]/th[contains(text(), "Launched")]/../td/text()').get())
+            competitor['industry'] = infobox_xpath.xpath(
+                './tbody/tr[*]/th[contains(text(), "Industry")]/../td[@class="category"]//text()').getall()
+            # clean industry values
+            competitor['industry'] = list(filter(lambda text: text != '\n', competitor['industry']))
+
+            try:
+                competitor['products'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Products")]/ancestor::tr/td/text()').get().split(', ')
+            except AttributeError:
+                competitor['products'] = None
+
+            except AttributeError:
+                competitor['number_of_employees'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Number of employees") or contains(text(), "Employees")]/ancestor::tr/td/text()').get()
+                competitor['number_of_employees'] = re.sub('[^0-9]', '', competitor['number_of_employees'])
+
+            except AttributeError:
+                competitor['number_of_employees'] = None
+                # acquisition['location'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Headquarters")]/ancestor::tr/td/string()').get()
+            competitor['summary'] = ''.join(response.xpath('//*[@id="mw-content-text"]/div/p[*]//text()').getall())
+            # competitor['number_of_employees']
+            line = competitor['summary'].text()
+            import re
+            match = re.search('(\d+) employees', line)
+            if match:
+                competitor['number_of_employees'] = match.group(1)
+
+
+
+        else:
+            competitor['founded'] = re.sub(r'[^\x00-\x7F]+', ' ', infobox_xpath.xpath('./tbody/tr[*]/th[contains(text(), "Founded")]/../td/text()').get())
+            competitor['industry'] = infobox_xpath.xpath('./tbody/tr[*]/th[contains(text(), "Industry")]/../td[@class="category"]//text()').getall()
+            # clean industry values
+            competitor['industry'] = list(filter(lambda text: text != '\n', competitor['industry']))
+            # some don't have products
+            try:
+               competitor['products'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Products")]/ancestor::tr/td/text()').get().split(', ')
+            except AttributeError:
+                competitor['products'] = None
+
+            except AttributeError:
+               competitor['number_of_employees'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Number of employees") or contains(text(), "Employees")]/ancestor::tr/td/text()').get()
+               competitor['number_of_employees'] = re.sub('[^0-9]','', competitor['number_of_employees'])
+            except AttributeError:
+               competitor['number_of_employees'] = None
+         # acquisition['location'] = infobox_xpath.xpath('./tbody/tr[*]//*[contains(text(), "Headquarters")]/ancestor::tr/td/string()').get()
+            competitor['summary'] = ''.join(response.xpath('//*[@id="mw-content-text"]/div/p[*]/b[contains(text(), "{}")]/..//text()'.format(title)).getall())
