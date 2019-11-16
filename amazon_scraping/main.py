cba345b920734b7a759f19b54891156fa1db9f7b
Matt Bond <m1bond@eng.ucsd.edu>
2019-11-16 09:52:10 -0800

    hi

--- amazon_scraping/main.py
+++ amazon_scraping/main.py
@@ -8,6 +8,29 @@ from scrapy.utils.project import get_project_settings
 # used just for human readable json
 import json
 
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+import pandas as pd
+# from amazon_scraping.spiders.acquisitions import AcquisitionsSpider
+
+# used just for human readable json
+import json
+import re
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+
+import os
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+import matplotlib.pyplot as plt
+
+
+
 scrapy_settings = get_project_settings()
 # modify scrapy_settings here if needed
 
@@ -17,7 +40,23 @@ output = {
          }
 process = CrawlerProcess(settings=scrapy_settings)
 process.crawl("acquisitions", output=output)
+process.crawl("competitors", output=output)
 process.start()
 
 # just for debugging
-print(json.dumps(output["acquisitions"], indent=4))
+print(json.dumps(output["competitors"], indent=4))
+
+if not os.path.exists('Figures'):
+    os.mkdir('Figures')
+text = {}
+for j in output:
+    for i in output[j]:
+        text[i] = output[j][i]['summary'].strip('\n').lower()
+        wordcloud = WordCloud().generate(text[i])
+
+        # Display the generated image:
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+
+        plt.savefig(os.path.join('Figures', '{}_cloud.jpg'.format(i)), dpi=500)
+        # plt.show()
